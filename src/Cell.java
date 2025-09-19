1eb16b3ce0dfd109c4f44bdf0d314fab07e76e92
muhammadibrahim1-sketch <muhammad.ibrahim1@students.mq.edu.au>
2025-09-20 02:56:01 +1000

    week03: Task 4 – adding Cell class with paint()

---
+++ src/Cell.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Cell {
+    private final int row;
+    private final int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    /** Drawing this cell at (x,y) with given size. */
+    public void paint(Graphics g, int x, int y, int size, boolean highlighted) {
+        if (highlighted) {
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(x, y, size, size);
+        }
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
+    }
+
+    public int row() { return row; }
+    public int col() { return col; }
+}
