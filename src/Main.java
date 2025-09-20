50fad5a1d81d26e43543a82a25f8703951c29a03
muhammadibrahim1-sketch <muhammad.ibrahim1@students.mq.edu.au>
2025-09-21 00:56:38 +1000

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,37 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+        Main window = new Main();
+        window.run();
     }
 
     class Canvas extends JPanel {
-      Stage stage = new Stage();
-      public Canvas() {
-        setPreferredSize(new Dimension(720, 720));
-      }
+        Grid grid = new Grid();   // use Grid instead of Stage
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 720));
+        }
 
-      @Override
-      public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
-      }
+        @Override
+        public void paint(Graphics g) {
+            grid.paint(g, getMousePosition()); // pass mouse pos into Grid
+        }
     }
 
     private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
     }
 
     public void run() {
-      while(true) {
-        repaint();
-      }
+        while (true) {
+            repaint();
+        }
     }
 }
