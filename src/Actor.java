2342bc7aa845e5033dd84f564e79c70186684485
muhammadibrahim1-sketch <muhammad.ibrahim1@students.mq.edu.au>
2025-09-21 01:02:50 +1000

    Updated Actor.java

--- src/Actor.java
+++ src/Actor.java
@@ -2,9 +2,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public abstract class Actor {
-  Color color;
-  Cell loc;
+  protected Color color;   // each subclass will define its own color
+  protected Cell loc;      // where on the grid the actor is
 
+  // constructor to set location + color
+  public Actor(Cell loc, Color color) {
+    this.loc = loc;
+    this.color = color;
+  }
+
+  // paint method: draws actor in its cell
   public void paint(Graphics g) {
     g.setColor(color);
     g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
